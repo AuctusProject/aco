949f0a4a9a6a261fac1892e21437692ffa28105d
thiagomva <thiagomvaraujo@gmail.com>
2020-06-08 15:55:46 -0300

    Bug fix

--- web/src/pages/Trade.js
+++ web/src/pages/Trade.js
@@ -154,7 +154,7 @@ class Trade extends Component {
       <>
         <TradeMenu {...this.props} selectedOption={this.state.selectedOption} onSelectOption={this.onSelectOption} selectedExpiryTime={this.state.selectedExpiryTime} onSelectExpiryTime={this.onSelectExpiryTime} options={this.state.options} balances={this.state.balances}/>
         {!this.state.selectedOption && <TradeOptionsList {...this.props} selectedExpiryTime={this.state.selectedExpiryTime} selectedOption={this.state.selectedOption} onSelectOption={this.onSelectOption} options={this.state.options} balances={this.state.balances} orderBooks={this.state.orderBooks}></TradeOptionsList>}
-        {this.state.selectedOption && <div id="trade-app"></div>}
+        <div id="trade-app" className={!this.state.selectedOption ? "d-none" : ""}></div>
       </>}
     </div>
     
