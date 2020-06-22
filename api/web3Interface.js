f2e649b5d1d0f781b8946b314754eb1f8f107950
arinyguedes <arinyguedes@gmail.com>
2020-06-22 19:29:53 -0300

    update api

--- api/web3Interface.js
+++ api/web3Interface.js
@@ -90,19 +90,6 @@ const getDecimals = (token) => {
   });
 };
 
-const quoteOpyn = (opynExchange, opynToken, swappedToken, amount, isBuy) => {
-  return new Promise((resolve, reject) => {
-    let data = (isBuy ? "0xbec165e7" : "0x3ed9c978") + addressToData(opynToken) + addressToData(swappedToken) + numberToData(amount)
-    callEthereum("eth_call", {"to": opynExchange, "data": data}).then((result) => {
-      if (result) {
-        resolve(BigInt(result).toString(10));
-      } else {
-        resolve(null);
-      }
-    }).catch((err) => reject(err));
-  });
-};
-
 const listAcoTokens = () => {   
   return new Promise((resolve, reject) => { 
     return callEthereum("eth_getLogs", {"address": [process.env.ACO_FACTORY], "fromBlock": fromBlock, "topics": ["0x830ecf50af8281b7fc6c07ca94ed228468437e79a01755686fbb541d37103cb2"]}, null).then((result) => {
@@ -140,11 +127,12 @@ const listAcoTokens = () => {
 
 module.exports.opynQuote = (queryArguments) => { 
   return new Promise((resolve, reject) => {
-    if ((queryArguments.isBuy !== true && queryArguments.isBuy !== false) || !queryArguments.exchange || 
-      !queryArguments.token || !queryArguments.swappedToken || !queryArguments.amount) {
+    if (!queryArguments || !queryArguments.exchange || !queryArguments.token || !queryArguments.swappedToken || 
+      !queryArguments.amount || (queryArguments.isBuy !== true && queryArguments.isBuy !== false && 
+        queryArguments.isBuy !== "true" && queryArguments.isBuy !== "false") ) {
       resolve(null);
     } else {
-      let data = (queryArguments.isBuy ? "0xbec165e7" : "0x3ed9c978") + addressToData(queryArguments.token) + addressToData(queryArguments.swappedToken) + numberToData(queryArguments.amount);
+      let data = ((queryArguments.isBuy === true || queryArguments.isBuy === "true") ? "0xbec165e7" : "0x3ed9c978") + addressToData(queryArguments.token) + addressToData(queryArguments.swappedToken) + numberToData(BigInt(queryArguments.amount));
       callEthereum("eth_call", {"to": queryArguments.exchange, "data": data}).then((result) => {
         if (result) {
           resolve(BigInt(result).toString(10));
