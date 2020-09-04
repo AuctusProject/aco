c14be84f2dab98b6c8596c48bd709d40219ffb0e
thiagomva <thiagomvaraujo@gmail.com>
2020-09-04 15:06:46 -0300

    Ordebook loading

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './customSwal2.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 
