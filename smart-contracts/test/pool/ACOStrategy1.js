4e6e3af9dcbb65405da5ef41baa4806e12f323b0
thiagomva <thiagomvaraujo@gmail.com>
2020-09-09 17:34:49 -0300

    Pool factory tests

--- smart-contracts/test/pool/ACOStrategy1.js
+++ smart-contracts/test/pool/ACOStrategy1.js
@@ -3,3 +3,20 @@ const strategy1ABI = require("../../artifacts/ACOStrategy1.json");
 
 describe("ACOStrategy1", function() {
 });
+
+exports.createAcoStrategy1 = async () => {
+    let percentagePrecision = 100000;
+    let underlyingPriceAdjustPercentage = 0.05 * percentagePrecision;
+    let minOptionPricePercentage = 0.05 * percentagePrecision;
+    let tolerancePercentageToOraclePrice = 0.05 * percentagePrecision;
+    let orderSizePenaltyFactor = 50;
+    let orderSizeDampingFactor = 4;
+    let newStrategy = await (await ethers.getContractFactory("ACOStrategy1")).deploy(
+      underlyingPriceAdjustPercentage,
+      minOptionPricePercentage,
+      tolerancePercentageToOraclePrice,
+      orderSizePenaltyFactor,
+      orderSizeDampingFactor);
+    await newStrategy.deployed();
+    return newStrategy;
+  }
