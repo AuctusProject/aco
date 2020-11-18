38263f74426f38349becdadb5c197bd979cba2dc
arinyguedes <arinyguedes@gmail.com>
2020-11-18 18:09:22 -0300

    otc update

--- api/web3Interface.js
+++ api/web3Interface.js
@@ -1,14 +1,13 @@
 const Axios = require('axios');
 
 const fromBlock = "0x" + parseInt(process.env.FROM_BLOCK).toString(16);
-let web3Id = 0;
 
 const callEthereum = (method, methodData, secondParam = "latest") => {
   return new Promise((resolve, reject) => {
     Axios.post("https://" + process.env.CHAIN + ".infura.io/v3/" + process.env.INFURA_ID, 
       {
         "jsonrpc":"2.0",
-        "id": ++web3Id,
+        "id": Date.now(),
         "method": method,
         "params": ((secondParam !== null && secondParam !== undefined) ? [methodData, secondParam] : [methodData])
       }, 
@@ -116,10 +115,23 @@ const getDecimals = (token) => {
   });
 };
 
+const getTransaction = (transactionHash) => {
+  return new Promise((resolve, reject) => {
+    callEthereum("eth_getTransactionReceipt", transactionHash, null).then((result) => {
+      if (result && result.blockNumber) {
+        resolve(result);
+      } else {
+        resolve(null);
+      }
+    }).catch((err) => reject(err));
+  });
+};
+
 const listAcoTokens = () => {   
   return new Promise((resolve, reject) => { 
     return callEthereum("eth_getLogs", {"address": [process.env.ACO_FACTORY], "fromBlock": fromBlock, "topics": ["0x830ecf50af8281b7fc6c07ca94ed228468437e79a01755686fbb541d37103cb2"]}, null).then((result) => {
-      const response = [];
+      const acos = [];
+      const promises = [];
       if (result) {
         const size = 64;
         const now = Math.ceil((new Date()).getTime() / 1000);
@@ -142,11 +154,20 @@ const listAcoTokens = () => {
             event.underlying = ("0x" + result[k].topics[1].substring(26));
             event.strikeAsset = ("0x" + result[k].topics[2].substring(26));
             event.isCall = (parseInt(result[k].topics[3], 16) === 1);
-            response.push(event);
+            promises.push(getTransaction(result[k].transactionHash));
+            acos.push(event);
           }
         }
       }
-      resolve(response);
+      Promise.all(promises).then((txs) => 
+      {
+        const response = [];
+        for (let j = 0; j < acos.length; ++j) {
+          acos[j].creator = txs[j].from;
+          response.push(acos[j]);
+        }
+        resolve(response);
+      }).catch((err) => reject(err));
     }).catch((err) => reject(err));
   });
 };
