ffeb15785ed7d93e44c891659b3664d04eeb03f9
arinyguedes <arinyguedes@gmail.com>
2021-05-17 19:26:27 -0300

    adjustment

--- web/src/util/Zrx/zrxApi.js
+++ web/src/util/Zrx/zrxApi.js
@@ -3,7 +3,6 @@ import { toDecimals, zrxApiUrl } from '../constants'
 import BigNumber from 'bignumber.js'
 
 export const getSwapQuote = async (isBuy, option, acoAmount = null, acoPrice = null) => {
-  debugger
   let makerToken = (isBuy ? option.acoToken : option.strikeAsset)
   let takerToken = (isBuy ? option.strikeAsset : option.acoToken)
   const orders = await getOrders(makerToken, takerToken)
